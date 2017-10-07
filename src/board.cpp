926e92ad99d1b764dcfb109a77faa346e0b7f3a3
George Ho <georgh0021@gmail.com>
2017-10-06 21:04:41 -0400

    BLD made legal moves fcn

--- src/board.cpp
+++ src/board.cpp
@@ -42,21 +42,23 @@ void othelloBoard::displayBoard(int color) {
 
 // Display legal moves for player
 void othelloBoard::displayLegalMoves() {
-    char colChar = 'A';
-    char rowChar = '1';
+    std::string colCoord = "ABCDEFGH";
+    std::string rowCoord = "12345678";
+    int colNum = 0;
+    int rowNum = 0;
     int moveNum = 1;
 
     for (auto keyval : this->moves) {
-        index2coord(keyval.first, colChar, rowChar);
+        index2coord(keyval.first, colNum, rowNum);
         std::cout << "Legal move " << moveNum++ << ") "
-            << colChar << rowChar << " ";
+            << colCoord[colNum] << rowCoord[colNum] << " ";
 
         std::list<int> discsFlipped = keyval.second;
         std::cout << "will flip discs: ";
 
         for (int disc : discsFlipped) {
-            index2coord(disc, colChar, rowChar);
-            std::cout << colChar << rowChar << " " << std::endl;
+            index2coord(disc, colNum, rowNum);
+            std::cout << colCoord[colNum] << rowCoord[rowNum] << " " << std::endl;
         }
 
         std::cout << std::endl;
@@ -66,27 +68,67 @@ void othelloBoard::displayLegalMoves() {
 // Finds all legal moves, returning a hash table with possible moves as keys,
 // and a list of all pieces to be flipped as values.
 void othelloBoard::findLegalMoves(int color) {
-    const std::vector<int> directions = {-1, 1, -8, 8, -7, 7, -9, 9};
-    int oppColor = (color == 1) ? 2 : 1;
-
     for (int i = 0; i < 64; i++) {
-        if (this->positions[i] == 0 || this->positions[i] == oppColor) {
-            continue;
-        }
-        else if (positions[i] == color) {
+        if (positions[i] == color) {
+            // Check rows
+            findLegalMoveInDirection(i, color, -1, moves);
+            findLegalMoveInDirection(i, color, 1, moves);
 
-        }
-        else {
-            std::cout << "ERROR, WE SHOULD NEVER GET HERE!!!" << std::endl;
-            return;
+            // Check columns
+            findLegalMoveInDirection(i, color, -8, moves);
+            findLegalMoveInDirection(i, color, 8, moves);
+
+            // Check diagonals
+            findLegalMoveInDirection(i, color, -9, moves);
+            findLegalMoveInDirection(i, color, 9, moves);
+            findLegalMoveInDirection(i, color, -7, moves);
+            findLegalMoveInDirection(i, color, 7, moves);
         }
     }
 }
 
-// Helper function to find a legal move given a disc and its color and a
-// direction
-void othelloBoard::findLegalMoveInDirection(int disc, int color, int direction) {
+// Helper function to find a legal move given a disc, its color and a direction.
+// Stores legal move and flipped discs as a pair in the moves hash table.
+void othelloBoard::findLegalMoveInDirection(int &disc, int &color, int direction,
+        std::unordered_map<int, std::list<int>> &moves) {
+    std::pair<int, std::list<int>> legalMove;
+    std::list<int> flippedDiscs;
+    int currentSquare = 0;
+    int oppColor = (color == 1) ? 2 : 1;
+    int row1 = 0, col1 = 0, row2 = 0, col2 = 0;
 
+    for (int i = disc + direction; i < 64 || i > -1; i += direction) {
+        // Guard against wrapping around the board
+        index2coord(disc, col1, row1);
+        index2coord(i, col2, row2);
+        if (abs(col1 - col2) > 1 || abs(row1 - row2) > 1) {
+            break;
+        }
+
+        // Keep moving in given direction, remembering any discs of the
+        // opposite color. Break if we see any discs of our color.
+        currentSquare = this->positions[i];
+        if (currentSquare == color) {
+            break;
+        }
+        else if (currentSquare == oppColor) {
+            flippedDiscs.push_front(i);
+            continue;
+        }
+        // If we see an empty square, it is a valid move: insert it into the
+        // moves hash table. Second condition is to resolve edge case of
+        // disc immediately adjacent to original disc.
+        else if (currentSquare == 0 && this->positions[i - direction] == oppColor) {
+            if (moves.find(i) != moves.end()) {
+                moves.erase(i);
+            }
+
+            legalMove.first = i;
+            legalMove.second = flippedDiscs;
+            moves.insert(legalMove);
+            break;
+        }
+    }
 }
 
 // Update positions after a move
@@ -95,10 +137,7 @@ void othelloBoard::updateBoard() {
 }
 
 // Helper function to convert board square index to coordinate strings
-void othelloBoard::index2coord(int index, char &colChar, char &rowChar) {
-    std::string colCoord = "ABCDEFGH";
-    std::string rowCoord = "12345678";
-
-    colChar = colCoord[index % 8];
-    rowChar = rowCoord[index / 8];
+void othelloBoard::index2coord(int index, int &colNum, int &rowNum) {
+    colNum = index % 8;
+    rowNum = index / 8;
 }
--- src/board.hpp
+++ src/board.hpp
@@ -9,14 +9,14 @@
 
 class othelloBoard {
     public:
-        // positions specifies all pieces on the board. Squares on the board
-        // are indexed from 0 to 63, left to right, top to bottom.
+        // positions specifies all pieces on the board. Squares on the
+        // board are indexed from 0 to 63, left to right, top to bottom.
         // positions[i] is 1 for a black disc, 2 for a white disc
         std::vector<int> positions;
 
-        // moves is a hash table specifying all possible moves from the current
-        // board position. Possible moves are keys, and a list of all pieces to
-        // be flipped are values.
+        // moves is a hash table specifying all possible moves from the
+        // current board position. Possible moves are keys, and a list of
+        // all pieces to be flipped are values.
         std::unordered_map<int, std::list<int>> moves;
 
         // Constructor
@@ -28,19 +28,21 @@ class othelloBoard {
         // Display legal moves for player
         void displayLegalMoves();
 
-        // Finds all legal moves, returning a hash table with possible moves as
-        // keys, and a list of all pieces to be flipped as values.
+        // Finds all legal moves, returning a hash table with possible
+        // moves as keys, and a list of all pieces to be flipped as values.
         void findLegalMoves(int color);
 
-        // Helper function to find a legal move given a disc and its color and a
-        // direction
-        void findLegalMoveInDirection(int disc, int color, int direction);
+        // Helper function to find a legal move given a disc, its color and a direction.
+        // Stores legal move and flipped discs as a pair in the moves hash table.
+        void findLegalMoveInDirection(int &disc, int &color, int direction,
+                std::unordered_map<int, std::list<int>> &moves);
 
         // Update board after a move
         void updateBoard();
 
-        // Helper function to convert board square index to coordinate strings
-        void index2coord(int index, char &colChar, char &rowChar);
+        // Helper function to convert board square index to coordinate
+        // strings
+        void index2coord(int index, int &colNum, int &rowNum);
 };
 
 #endif // BOARD_HPP

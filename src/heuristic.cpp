172e2760a736a3457fa8dc0a4d05a763a7168fda
George Ho <georgh0021@gmail.com>
2017-10-09 01:11:13 -0400

    BLD skeleton of heuristic

---
+++ src/heuristic.cpp
@@ -0,0 +1,21 @@
+#include "heuristic.hpp"
+
+int othelloHeuristic::evaluate(const othelloBoard &board, int &color) {
+    return 0;
+}
+
+int othelloHeuristic::discDifference(const othelloBoard &board, int &color) {
+    return 0;
+}
+
+int othelloHeuristic::mobility(const othelloBoard &board, int &color) {
+    return 0;
+}
+
+int othelloHeuristic::stability(const othelloBoard &board, int &color) {
+    return 0;
+}
+
+int othelloHeuristic::parity(const othelloBoard &board, int &color) {
+    return 0;
+}
---
+++ src/heuristic.hpp
@@ -0,0 +1,22 @@
+#ifndef HEURISTIC_HPP
+#define HEURISTIC_HPP
+
+/* References:
+ * http://mkorman.org/othello.pdf
+ * https://www.tjhsst.edu/~rlatimer/techlab10/Per5/FourthQuarter/ChenPaperQ4-10.pdf
+ */
+
+#include "board.hpp"
+
+class othelloHeuristic {
+    public:
+        int evaluate(const othelloBoard &board, int &color);
+
+    private:
+        int discDifference(const othelloBoard &board, int &color);
+        int mobility(const othelloBoard &board, int &color);
+        int stability(const othelloBoard &board, int &color);
+        int parity(const othelloBoard &board, int &color);
+};
+
+#endif // HEURISTIC_HPP

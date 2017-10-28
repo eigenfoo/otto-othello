a0071954bb35b3dd2a45fd174fc4f993c62cddf7
George Ho <georgh0021@gmail.com>
2017-10-27 23:06:35 -0400

    BLD ai operational.

--- src/heuristic.cpp
+++ src/heuristic.cpp
@@ -1,97 +1,98 @@
 #include "heuristic.hpp"
-// Heuristic is from the perspective that black maximizes
-// TODO the heuristic should change as the game progresses!
+// Heuristic is from the perspective that the calling player is max,
+// regardless of the player's color
 
 int othelloHeuristic::evaluate(othelloBoard &board, int color) {
     if (board.terminalState()) {
-        return 100000*utility(board);
+        return 100000*utility(board, color);
     }
 
-    discDifferenceScore = discDifference(board);
-    mobilityScore = mobility(board, color);
-    potentialMobilityScore = potentialMobility(board, color);
     stabilityScore = stability(board, color);
-    parityScore = parity(board, color);
+    cornerScore = corners(board, color);
 
     if (board.discsOnBoard <= 20) {
         // Opening game
-        return 5*mobilityScore
-            + 2*potentialMobilityScore
+        mobilityScore = mobility(board, color);
+        squareWeightsScore = squareWeights(board, color);
+
+        return 10*mobilityScore
+            + 20*squareWeightsScore
+            + 1000*cornerScore
             + 1000*stabilityScore;
     }
     else if (board.discsOnBoard <= 58) {
         //Midgame
+        discDifferenceScore = discDifference(board, color);
+        mobilityScore = mobility(board, color);
+        squareWeightsScore = squareWeights(board, color);
+
         return discDifferenceScore
-            + 5*mobilityScore
-            + 2*potentialMobilityScore
-            + 1000*stabilityScore
-            + 500*parityScore;
+            + 10*mobilityScore
+            + 20*squareWeightsScore
+            + 1000*cornerScore
+            + 1000*stabilityScore;
     }
     else {
+        discDifferenceScore = discDifference(board, color);
+
         // Endgame
         return 500*discDifferenceScore
-            + 1000*stabilityScore
-            + 2000*parityScore;
+            + 1000*cornerScore
+            + 1000*stabilityScore;
     }
 }
 
-int othelloHeuristic::utility(othelloBoard &board) {
-    return std::accumulate(board.positions.begin(), board.positions.end(), 0);
+int othelloHeuristic::utility(othelloBoard &board, int &color) {
+    int util = std::accumulate(board.positions.begin(), board.positions.end(), 0);
+
+    if (color == 1) {
+        return util; 
+    }
+    else {
+        return -1*util;
+    }
 }
 
 // Relative disc difference between the two players
-int othelloHeuristic::discDifference(othelloBoard &board) {
+int othelloHeuristic::discDifference(othelloBoard &board, int &color) {
     // Number of black discs
-    foo = std::count(board.positions.begin(),
+    int blackCount = std::count(board.positions.begin(),
             board.positions.end(), 1);
 
     // Number of white discs
-    bar = std::count(board.positions.begin(),
+    int whiteCount = std::count(board.positions.begin(),
             board.positions.end(), -1);
-    
-    if (foo > bar) {
-        discDifferenceScore = (100 * foo) / (foo + bar);
-    }
-    else if (foo < bar) {
-        discDifferenceScore = (-100 * bar) / (foo + bar);
+
+    if (color == 1) {
+        return 100 * (blackCount - whiteCount) / (blackCount + whiteCount);
     }
     else {
-        discDifferenceScore = 0;
+        return 100 * (whiteCount - blackCount) / (blackCount + whiteCount);
     }
-
-    return discDifferenceScore;
 }
 
 // Number of possible moves
 int othelloHeuristic::mobility(othelloBoard &board, int &color) {
     // Black mobility
     board.findLegalMoves(1, &pMoves);
-    foo = pMoves.size();
+    int blackMoves = pMoves.size();
     pMoves.clear();
 
     // White mobility
     board.findLegalMoves(-1, &pMoves);
-    bar = pMoves.size();
+    int whiteMoves = pMoves.size();
     pMoves.clear();
 
-    mobilityScore = 0;
-
-    if (foo > bar) {
-        mobilityScore = (100*foo) / (foo + bar);
+    if (color == 1) {
+        return 100 * (blackMoves - whiteMoves) / (blackMoves + whiteMoves + 1);
     }
-    else if (foo < bar) {
-        mobilityScore = (-100*bar) / (foo + bar);
+    else {
+        return 100 * (whiteMoves - blackMoves) / (blackMoves + whiteMoves + 1);
     }
-
-    return mobilityScore;
-}
-
-int othelloHeuristic::potentialMobility(othelloBoard &board, int &color) {
-    return 0;
 }
 
 // Computes a lower bound on the number of stable discs
-int othelloHeuristic::stability(othelloBoard &board, int &color) {
+int othelloHeuristic::stability(othelloBoard &board, int color) {
     stableDiscs.clear();
 
     stableDiscsFromCorner(board, 0, color);
@@ -99,12 +100,21 @@ int othelloHeuristic::stability(othelloBoard &board, int &color) {
     stableDiscsFromCorner(board, 56, color);
     stableDiscsFromCorner(board, 63, color);
 
-    return stableDiscs.size();
+    int myStable = stableDiscs.size();
+
+    stableDiscsFromCorner(board, 0, -color);
+    stableDiscsFromCorner(board, 7, -color);
+    stableDiscsFromCorner(board, 56, -color);
+    stableDiscsFromCorner(board, 63, -color);
+
+    int opponentStable = stableDiscs.size();
+
+    return myStable - opponentStable;
 }
 
 // Finds the number of stable discs given a corner
 void othelloHeuristic::stableDiscsFromCorner(othelloBoard &board, int corner,
-        int &color) {
+        int color) {
 
     bool down, right;
     if (corner == 0) {
@@ -160,29 +170,8 @@ void othelloHeuristic::stableDiscsFromCorner(othelloBoard &board, int corner,
     }
 }
 
-// Who is expected to have the last move in the game
-int othelloHeuristic::parity(othelloBoard &board, int &color) {
-    if (color == 1) {
-        if (board.discsOnBoard % 2 == 0) {
-            parityScore = -1;
-        }
-        else {
-            parityScore = 1;
-        }
-    }
-    else {
-        if (board.discsOnBoard % 2 == 0) {
-            parityScore = -1;
-        }
-        else {
-            parityScore = 1;
-        }
-    }
-    return parityScore;
-}
-
 // Assigns a weight to every square on the board
-int othelloHeuristic::squareWeights(othelloBoard &board) {
+int othelloHeuristic::squareWeights(othelloBoard &board, int &color) {
     std::vector<int> weights = {
          100, -100, 100,  50,  50, 100, -100,  100,
         -100, -200, -50, -50, -50, -50, -200, -100,
@@ -208,6 +197,7 @@ int othelloHeuristic::squareWeights(othelloBoard &board) {
         weights[24] = 0;
         weights[25] = 0;
     }
+
     if (board.positions[7] != 0) {
         weights[4]  = 0;
         weights[5]  = 0;
@@ -222,6 +212,7 @@ int othelloHeuristic::squareWeights(othelloBoard &board) {
         weights[30] = 0;
         weights[31] = 0;
     }
+
     if (board.positions[56] != 0) {
         weights[32] = 0;
         weights[33] = 0;
@@ -236,6 +227,7 @@ int othelloHeuristic::squareWeights(othelloBoard &board) {
         weights[58] = 0;
         weights[59] = 0;
     }
+
     if (board.positions[63] != 0) {
         weights[38] = 0;
         weights[39] = 0;
@@ -251,6 +243,36 @@ int othelloHeuristic::squareWeights(othelloBoard &board) {
         weights[62] = 0;
     }
 
-    return std::inner_product(board.positions.begin(), board.positions.end(),
-            weights.begin(), 0);
+    if (color == 1) {
+        return std::inner_product(board.positions.begin(), board.positions.end(),
+                weights.begin(), 0);
+    }
+    else {
+        return -1*std::inner_product(board.positions.begin(), board.positions.end(),
+                weights.begin(), 0);
+    }
+}
+
+int othelloHeuristic::corners(othelloBoard &board, int &color) {
+    std::vector<int> corners = {0, 7, 56, 63};
+    int blackCorners = 0;
+    int whiteCorners = 0;
+
+    for (int corner : corners) {
+        if (board.positions[corner] == 1) {
+            blackCorners++;
+        }
+        else if (board.positions[corner] == -1) {
+            whiteCorners++;
+        }
+    }
+
+    if (color == 1) {
+        return 100 * (blackCorners - whiteCorners)
+            / (blackCorners + whiteCorners + 1);
+    }
+    else {
+        return 100 * (whiteCorners - blackCorners)
+            / (blackCorners + whiteCorners + 1);
+    }
 }
